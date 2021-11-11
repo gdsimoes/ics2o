ec6bcfc0a3b1a077d5a85981b21990d5a45ea58b
Guilherme Dias Simoes <gds.simoes@gmail.com>
2021-11-11 15:03:00 +0000

    Add some functions.

--- unit3/searchAndSort.c
+++ unit3/searchAndSort.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+void print_array(int arr[], int size);
+void swap(int *a, int *b);
+
 int linear_search(int arr[], int size, int elem);
 int binary_search(int arr[], int size, int elem);
 
-void bubbleSort(int arr[]);
+void bubbleSort(int arr[], int size);
 
 int main(void)
 {
@@ -16,6 +19,44 @@ int main(void)
         printf("%i\n", binary_search(sorted, 8, i));
     }
 
+    print_array(sorted, 8);
+    print_array(unsorted, 8);
+
+    int a = 1;
+    int b = 2;
+
+    swap(&a, &b);
+
+    printf("%i %i\n", a, b);
+
+
+}
+
+// This function prints all elements of an array in a pretty way
+void print_array(int arr[], int size)
+{
+    if (size == 0)
+    {
+        printf("[]\n");
+    }
+    else
+    {
+        printf("[");
+        for (int i = 0; i < size - 1; i++)
+        {
+            printf("%i, ", arr[i]);
+        }
+        printf("%i]\n", arr[size - 1]);
+    }
+}
+
+void swap(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 
 }
 
@@ -23,26 +64,35 @@ int linear_search(int arr[], int size, int elem)
 {
     for (int i = 0; i < size; i++)
     {
+        // If found return that index
         if (arr[i] == elem)
         {
             return i;
         }
     }
 
+    // Since -1 is never an index it is a good value to signal that the element wasn't found
     return -1;
 }
 
 int binary_search(int arr[], int size, int elem)
 {
+    // a and b representing the beginning and the end (not including b)
     int a = 0;
     int b = size;
 
-    while (b - a > 0)
+    // We are going to continue searching while the beginning is less than the end
+    while (a < b)
     {
+        // This is integer division
+        const int middle = (a + b)/2;
+
+        // If found just return the index
         if (arr[(a + b)/2] == elem)
         {
             return (a + b)/2;
         }
+        // Otherwise restrict the search to the correct half of the terminal
         else if (arr[(a + b)/2] > elem)
         {
             b = (a + b)/2;
@@ -56,7 +106,11 @@ int binary_search(int arr[], int size, int elem)
     return -1;
 }
 
-void sort(int arr[])
+void bubbleSort(int arr[], int size)
 {
+    for (int i = 0; i < size - 1; i++)
+    {
+
+    }
     return;
 }
